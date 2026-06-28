b89bfb64b3581966ba970e5853d4fce7f11fa1df
rohitdhum <rohit45dhumal@gmail.com>
2026-06-28 22:43:23 +0530

    Add files via upload

---
+++ UserdefinedFunction9.py
@@ -0,0 +1,14 @@
+def BigBazar():
+    print("Inside BigBazar")
+
+    def Amul():
+        print("Inside Amul IceCream Parlor")
+
+    Amul()
+    Amul()
+    
+def main():
+    BigBazar()          # Allowed
+
+if __name__ == "__main__":
+    main()
---
+++ VariableNumber.py
@@ -0,0 +1,9 @@
+def Display(*Data):
+    print(Data)
+    print(type(Data))
+
+def main():
+        Display(10,40.89,False,"Python")
+
+if __name__ == "__main__":
+    main()
